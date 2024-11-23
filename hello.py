20ba1f7fdf6d18cae945d475a6747ae69e86c7c6
milenchaves <milenchaves.study@gmail.com>
2024-11-23 18:54:05 -0300

    first crud

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from voluntariado-vaga!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from datetime import date
+
+class Vaga(BaseModel):
+    id_vaga : int
+    titulo : str
+    descricao : str
+    organizacao : str
+    data_publicacao : date
+    localizacao : str
