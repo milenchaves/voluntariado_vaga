e7eb4426b851ef76b136a6f68fdd82ea29fd4692
milenchaves <milenchaves.study@gmail.com>
2024-11-24 18:06:51 -0300

    rota home

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ CSV_FILE = "csv/vagas.csv"
 # Inicializando FastAPI
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"msg": "Vagas para voluntariado"}
+
 app.include_router(rota_vagas, prefix="/api", tags=["Vagas"])
